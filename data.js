68b239590f4f3ddaa1792a24ecf2ad5bb7f1c807
WeLee_Arjona <152079892+W3L33@users.noreply.github.com>
2026-01-29 13:39:06 -0500

    Update data.js

--- data.js
+++ data.js
@@ -5,6 +5,10 @@ const data = {
       Kadever: {
         link: "https://sites.google.com/colegioalamoscancun.edu.mx/kadever/inicio",
         image: "https://raw.githubusercontent.com/W3L33/alamos/refs/heads/main/imgp/arg_diego.png"
+      },
+        SuperDinamite: {
+        link: "https://sites.google.com/colegioalamoscancun.edu.mx/super/inicio",
+        image: "https://raw.githubusercontent.com/W3L33/alamos/refs/heads/main/imgp/arg_superdinamite.png"
       }
     }
   },
@@ -19,6 +23,10 @@ const data = {
          SuperKernel: {
         link: "https://sites.google.com/colegioalamoscancun.edu.mx/the-super-kernel/inicio?authuser=0",
         image: "https://raw.githubusercontent.com/W3L33/alamos/refs/heads/main/imgp/por_kernel.png"
+      },
+             CleanVibesss: {
+        link: "https://sites.google.com/colegioalamoscancun.edu.mx/restaurante-y-menu/inicio",
+        image: "https://raw.githubusercontent.com/W3L33/alamos/refs/heads/main/imgp/por_cleanvibes.png"
       }
     }
   },
@@ -29,6 +37,14 @@ const data = {
       FoodHunters: {
         link: "https://sites.google.com/colegioalamoscancun.edu.mx/food-hunters/inicio",
         image: "https://raw.githubusercontent.com/W3L33/alamos/refs/heads/main/imgp/uru_maren.png"
+      },
+            Powerpets: {
+        link: "https://sites.google.com/colegioalamoscancun.edu.mx/powerpets/inicio",
+        image: ""
+      },
+      BlackWidow: {
+        link: "https://sites.google.com/colegioalamoscancun.edu.mx/huntersproject/inicio?authuser=0",
+        image: "https://raw.githubusercontent.com/W3L33/alamos/refs/heads/main/imgp/uru_blackwidow.png"
       }
     }
   },
@@ -54,5 +70,6 @@ const data = {
   }
 
 };
+
 
 
